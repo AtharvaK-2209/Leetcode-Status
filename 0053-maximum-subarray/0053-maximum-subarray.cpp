bd56baf4049d54b900a6332a6015cf6d4e6c0bf7
Atharva Kalam <pictxatharva22@gmail.com>
2026-06-25 12:58:31 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum = INT_MIN;
-        int win_sum = 0;
-        for(int i = 0;i<nums.size();i++){
-            win_sum += nums[i];
-            max_sum = max(max_sum,win_sum);
-            if(win_sum < 0){
-                win_sum = 0;
+        int n = nums.size();
+        int maxSum = INT_MIN;
+        
+        // Brute Force Approach : Calculate maxSum for every possible subarray at index i
+        // for(int i = 0; i< n; i++){
+        //     int currentSum = 0;
+        //     for( int j = i ; j < n ;j++){
+        //         currentSum += nums[j];
+
+        //         maxSum = max(maxSum, currentSum);
+        //     }
+        // }
+
+        // Optimal Approach : Kdane's Algorithm
+        int currentSum = 0;
+        for(int i = 0 ; i < n ; i++){
+            currentSum += nums[i];
+            maxSum = max(maxSum, currentSum);
+
+            if(currentSum < 0) {
+                currentSum = 0;
             }
+
+            
         }
-        return max_sum;
+
+        return maxSum;
     }
 };
