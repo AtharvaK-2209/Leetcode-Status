822ca64ec0f7660c92458a7063c229d3ba02ee26
Atharva Kalam <pictxatharva22@gmail.com>
2026-06-27 13:35:30 +0530

    Time: 2 ms (50.82%), Space: 9.6 MB (75.83%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Brute Force : Sorting the array. Time : O(n log n)
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if(s == t ) return true;
+        // return false;
+
+        // Optimal : Using Frequency array since only English letters are present in strings. Time : O(n). If the inputs contain Unicode or arbitrary Characters we will think for unordered_map<char, int>
+
+        int freq[26] = {0};
+
+        for(char c : s){
+            freq[c-'a']++;
+        }
+        for(char c : t){
+            freq[c-'a']--;
+        }
+
+        for(int count : freq){
+            if(count != 0)
+                return false;
+        }
+
+        return true;
+
+    }
+};
