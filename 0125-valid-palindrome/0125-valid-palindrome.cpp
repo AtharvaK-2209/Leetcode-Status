ad6acea25f6a313a4be62a786dd17ac8beecd741
Atharva Kalam <pictxatharva22@gmail.com>
2026-06-29 19:53:22 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         // Brute Force Approach : Use clean string to store only letter and digit then check for palindrome
-        string clean = "";
-        for(char c : s){
-            if(isalnum(c)) 
-                clean += tolower(c);
-        }
-        int left = 0, right = clean.size()-1;
+        // string clean = "";
+        // for(char c : s){
+        //     if(isalnum(c)) 
+        //         clean += tolower(c);
+        // }
+        // int left = 0, right = clean.size()-1;
+        // while(left < right){
+        //     if(clean[left] != clean[right]) return false;        
+        //     left ++;
+        //     right --;
+        // }
+        // return true;
+
+        // Optimal Approach : Direct 2 pointers in original string :
+       int left = 0, right = s.size()-1;
         while(left < right){
-            if(clean[left] != clean[right]) return false;        
-            left ++;
-            right --;
+            while(left < right && !isalnum(s[left]))
+                left++;
+            while(left < right && !isalnum(s[right]))
+                right--;
+
+            if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
         }
         return true;
+
+
     }
 };
