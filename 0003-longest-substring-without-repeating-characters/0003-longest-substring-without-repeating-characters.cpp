43c855c36db2ab0c9bd807f7e08dd8dac4df31d4
Atharva Kalam <pictxatharva22@gmail.com>
2026-07-06 22:44:54 +0530

    Time: 7 ms (63.83%), Space: 11.7 MB (65.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int longest = 0;
+        // Brute Force : Generate every possible substring. For each substring, check whether it contains duplicate characters.If not, update the maximum length.
 
-        for(int i = 0; i < n ; i++){
-            unordered_set<char> st;
-            for(int j = i; j < n ; j++){
-                if(st.count(s[j]))
-                    break;
-                st.insert(s[j]);
-                longest = max(longest, j-i+1);
+        // int n = s.size();
+        // int longest = 0;
+        // for(int i = 0; i < n ; i++){
+        //     unordered_set<char> st;
+        //     for(int j = i; j < n ; j++){
+        //         if(st.count(s[j]))
+        //             break;
+        //         st.insert(s[j]);
+        //         longest = max(longest, j-i+1);
+        //     }
+        // }
+        // return longest;
+
+        // Optimal Approach : Sliding Window + HashMap
+
+        unordered_map < char, int> mp;
+        int left = 0 , longest = 0;
+        for(int right = 0 ; right < s.size(); right++){
+            if(mp.count(s[right])){
+                left = max(left, mp[s[right]] + 1);
             }
+            mp[s[right]] = right;
+            longest = max(longest, right - left + 1);
         }
         return longest;
     }
