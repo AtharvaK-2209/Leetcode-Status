4dfe2a8ba9a1fb400eb08e8b5162b38707c80471
Atharva Kalam <pictxatharva22@gmail.com>
2026-07-24 23:59:42 +0530

    Time: 25 ms (72.16%), Space: 16.2 MB (90.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int row, int col) {
+
+    void dfs (vector<vector<char>> &grid, int row, int col){
         int m = grid.size();
         int n = grid[0].size();
-        // Boundary check
-        if(row < 0 || col < 0 || row >= m || col >= n)
-            return;
-        // Stop if water or already visited
-        if(grid[row][col] == '0')
-            return;
-        // Mark current land as visited
-        grid[row][col] = '0';
-        // Explore all 4 directions
-        dfs(grid, row - 1, col);
-        dfs(grid, row + 1, col);
-        dfs(grid, row, col - 1);
-        dfs(grid, row, col + 1);
-    }
 
+        // boundry condition
+        if(row < 0 || col < 0 || row >= m || col >= n) return;
+
+        // if current node is already visited or is water
+        if(grid[row][col] == '0') return;
+
+        //checking neighbors 
+        grid[row][col] = '0';
+        dfs( grid, row - 1, col);
+        dfs( grid, row + 1, col);
+        dfs( grid, row, col - 1);
+        dfs( grid, row, col + 1);
+
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0;
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                if(grid[i][j] == '1') {
+        int row = grid.size();
+        int col = grid[0].size();
+        int count = 0 ; 
+        for ( int i = 0 ; i < row ; i++ ){
+            for(int j = 0 ; j < col ; j++ ){
+                if(grid[i][j] == '1'){
                     count++;
                     dfs(grid, i, j);
                 }
