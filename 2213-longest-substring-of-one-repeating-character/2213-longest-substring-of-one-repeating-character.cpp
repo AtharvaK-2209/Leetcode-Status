c38d8b21a6e71efbe8fec7bf9abdca9d71532067
Atharva Kalam <pictxatharva22@gmail.com>
2026-08-13 21:49:35 +0530

    Time: 324 ms (36.24%), Space: 113.7 MB (25.51%) - LeetHub

---
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,107 @@
+class Solution {
+public:
+    struct Node {
+        int len;
+        int pref, suff, best;
+        char leftChar, rightChar;
+
+        Node() {
+            len = pref = suff = best = 0;
+            leftChar = rightChar = '#';
+        }
+    };
+
+    vector<Node> seg;
+    string s;
+
+    Node merge(Node a, Node b) {
+        if (a.len == 0) return b;
+        if (b.len == 0) return a;
+
+        Node res;
+        res.len = a.len + b.len;
+
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+        res.pref = a.pref;
+        res.suff = b.suff;
+        res.best = max(a.best, b.best);
+
+        if (a.rightChar == b.leftChar) {
+            res.best = max(res.best, a.suff + b.pref);
+
+            if (a.pref == a.len)
+                res.pref = a.len + b.pref;
+
+            if (b.suff == b.len)
+                res.suff = b.len + a.suff;
+        }
+
+        return res;
+    }
+
+    void build(int idx, int l, int r) {
+        if (l == r) {
+            seg[idx].len = 1;
+            seg[idx].pref = 1;
+            seg[idx].suff = 1;
+            seg[idx].best = 1;
+            seg[idx].leftChar = s[l];
+            seg[idx].rightChar = s[l];
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(2 * idx, l, mid);
+        build(2 * idx + 1, mid + 1, r);
+
+        seg[idx] = merge(seg[2 * idx], seg[2 * idx + 1]);
+    }
+
+    void update(int idx, int l, int r, int pos, char c) {
+        if (l == r) {
+            seg[idx].leftChar = c;
+            seg[idx].rightChar = c;
+            seg[idx].pref = 1;
+            seg[idx].suff = 1;
+            seg[idx].best = 1;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (pos <= mid)
+            update(2 * idx, l, mid, pos, c);
+        else
+            update(2 * idx + 1, mid + 1, r, pos, c);
+
+        seg[idx] = merge(seg[2 * idx], seg[2 * idx + 1]);
+    }
+
+    vector<int> longestRepeating(string str, string queryCharacters,
+                                 vector<int>& queryIndices) {
+        s = str;
+        int n = s.size();
+
+        seg.resize(4 * n);
+
+        build(1, 0, n - 1);
+
+        vector<int> ans;
+
+        for (int i = 0; i < queryIndices.size(); i++) {
+            int pos = queryIndices[i];
+            char c = queryCharacters[i];
+
+            s[pos] = c;
+
+            update(1, 0, n - 1, pos, c);
+
+            ans.push_back(seg[1].best);
+        }
+
+        return ans;
+    }
+};
